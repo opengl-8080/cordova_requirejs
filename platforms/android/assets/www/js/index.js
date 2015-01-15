6896f4153226e14cfd37ada1633ced65d6dd8f2e
opengl8080 <tomcat.port.8080+github@gmail.com>
2015-01-15 23:55:42 +0900

    refactor

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -10,12 +10,14 @@ require(['root', 'database', 'jquery-mobile'], function(root) {
         root.log('Hello Cordova with RequireJS!!');
 
         root.db.readTransaction(function(tx) {
-            tx.executeSql('SELECT * FROM SAMPLE_TABLE', [], function(tx, result) {
-                for (var i=0; i<result.rows.length; i++) {
-                    var row = result.rows.item(i);
-                    root.log('[database rows] id=' + row.ID + ', message=' + row.MESSAGE);
-                }
-            });
+            root.deferredTransaction(tx)
+                .first('SELECT * FROM SAMPLE_TABLE')
+                .done(function(exec, result) {
+                    for (var i=0; i<result.rows.length; i++) {
+                        var row = result.rows.item(i);
+                        root.log('[database rows] id=' + row.ID + ', message=' + row.MESSAGE);
+                    }
+                });
         });
     });
 });
